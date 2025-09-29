7730acd0b1d5074e6a73fe7735eabd6592636be8
Casey Parker <02-germane-atomic@icloud.com>
2025-09-29 12:07:44 -0700

    modified:   .github/workflows/Windows.yml 	modified:   build/build.bat

--- build/build.bat
+++ build/build.bat
@@ -3,21 +3,6 @@
 :: Change to the directory of this script
 cd /d "%~dp0"
 
-:: Prefer venv python if available
-if exist "..\.venv\Scripts\python.exe" (
-    set PYTHON_EXE=..\venv\Scripts\python.exe
-) else (
-    set PYTHON_EXE=python
-)
-
-:: Check if python is installed
-where %PYTHON_EXE% >nul 2>nul
-
-if errorlevel 1 (
-    echo Python is not installed.
-    exit /b 1
-)
-
 :: Run build.py if it exists, passing the first argument if provided
 if exist "build.py" (
     %PYTHON_EXE% .\build.py %*
