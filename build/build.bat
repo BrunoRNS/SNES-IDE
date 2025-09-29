015b6c4017a07b602b39cb5ec193e0b9b5548737
Casey Parker <02-germane-atomic@icloud.com>
2025-09-29 11:33:15 -0700

    make some sanity

--- build/build.bat
+++ build/build.bat
@@ -3,25 +3,25 @@
 :: Change to the directory of this script
 cd /d "%~dp0"
 
-:: Check if python is installed
+:: Prefer venv python if available
+if exist "..\.venv\Scripts\python.exe" (
+    set PYTHON_EXE=..\venv\Scripts\python.exe
+) else (
+    set PYTHON_EXE=python
+)
 
-where python >nul 2>nul
+:: Check if python is installed
+where %PYTHON_EXE% >nul 2>nul
 
 if errorlevel 1 (
-
     echo Python is not installed.
     exit /b 1
-
 )
 
 :: Run build.py if it exists, passing the first argument if provided
 if exist "build.py" (
-
-    python .\build.py %*
-
+    %PYTHON_EXE% .\build.py %*
 ) else (
-
     echo build.py not found.
     exit /b 1
-
 )
